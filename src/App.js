6644b009f8d4b6df0fb2458a8aadfeb8b8f0dcc0
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-10-22 11:10:02 -0700

    form setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from "react";
+import { Button, Divider, Form, Grid, Segment } from "semantic-ui-react";
 
 function App() {
-  return <div>
+  return (
+    <div>
+      <h1>Hello,</h1>
+      <h2>Welcome to our game!</h2>
+      <Segment placeholder>
+        <Grid columns={2} relaxed="very" stackable>
+          <Grid.Column>
+            <Form>
+              <Form.Input
+                icon="user"
+                iconPosition="left"
+                label="Username"
+                placeholder="Username"
+              />
+              <Form.Input
+                icon="lock"
+                iconPosition="left"
+                label="Password"
+                type="password"
+              />
 
-    <h1>CS Build week 1</h1>
-    <h2>Hector - Artin - Jacob - Christian</h2>
-    <p>testing production deployment</p>
-  </div>;
+              <Button content="Login" primary />
+            </Form>
+          </Grid.Column>
+
+          <Grid.Column verticalAlign="middle">
+            <Button content="Register!" icon="signup" size="big" />
+          </Grid.Column>
+        </Grid>
+
+        <Divider vertical>Or</Divider>
+      </Segment>
+      <p>CS Build week 1</p>
+      <p>Hector - Artin - Jacob - Christian</p>
+    </div>
+  );
 }
 
 export default App;
