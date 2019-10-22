a76179c7f0eb0e7c1e6d4f691c1b1b5e56f631c9
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-10-22 11:42:12 -0700

    react router setup

---
+++ src/components/Login.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Button, Divider, Form, Grid, Segment } from "semantic-ui-react";
+
+export default function Login() {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleInputChange = event => {
+    event.persist();
+    setInputs(inputs => ({
+      ...inputs,
+      [event.target.id]: event.target.value
+    }));
+    console.log(inputs);
+  };
+
+  const handleLogin = event => {
+    event.preventDefault();
+
+    axios
+      .post("https://lambda-mud-test.herokuapp.com/api/login/", inputs)
+      .then(response => {
+        localStorage.setItem("csbuildweek1", response.data.key);
+        setInputs({
+          username: "",
+          password: ""
+        });
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Hello,</h1>
+      <h2>Welcome to our game!</h2>
+      <Segment placeholder>
+        <Grid columns={2} relaxed="very" stackable>
+          <Grid.Column>
+            <Form onSubmit={handleLogin}>
+              <Form.Input
+                icon="user"
+                iconPosition="left"
+                label="Username"
+                placeholder="Username"
+                id="username"
+                onChange={handleInputChange}
+                value={inputs.username}
+              />
+              <Form.Input
+                icon="lock"
+                iconPosition="left"
+                label="Password"
+                type="password"
+                id="password"
+                onChange={handleInputChange}
+                value={inputs.password}
+              />
+
+              <Button content="Login" primary />
+            </Form>
+          </Grid.Column>
+
+          <Grid.Column verticalAlign="middle">
+            <Button content="Register!" icon="signup" size="big" />
+          </Grid.Column>
+        </Grid>
+
+        <Divider vertical>Or</Divider>
+      </Segment>
+      <p>CS Build week 1</p>
+      <p>Hector - Artin - Jacob - Christian</p>
+    </div>
+  );
+}
