dae35c87fad245c33e7ce28a907b1c208effc4c3
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-10-22 13:02:52 -0700

    log out in game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Button } from "semantic-ui-react";
 
-export default function Game() {
+export default function Game(props) {
+  const handleLogout = () => {
+    localStorage.removeItem("csbuildweek1");
+    props.history.push("/");
+  };
+
   return (
     <div>
+      <Button onClick={handleLogout}>Log out</Button>
       <h1>GAME</h1>
     </div>
   );
