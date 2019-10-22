8ead02bfcae629da7f640e8bf8d4630367a16fe9
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-10-22 14:03:31 -0700

    game component useEffect

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,23 @@
-import React from "react";
-import { Button } from "semantic-ui-react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { Button, Icon } from "semantic-ui-react";
 
 export default function Game(props) {
+  useEffect(() => {
+    axios
+      .get("https://lambda-mud-test.herokuapp.com/api/adv/init/", {
+        headers: {
+          Authorization: `Token ${localStorage.getItem("csbuildweek1")}`
+        }
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  }, []);
+
   const handleLogout = () => {
     localStorage.removeItem("csbuildweek1");
     props.history.push("/");
@@ -9,8 +25,13 @@ export default function Game(props) {
 
   return (
     <div>
-      <Button onClick={handleLogout}>Log out</Button>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Button onClick={handleLogout}>Log out</Button>
+      </div>
+
       <h1>GAME</h1>
+
+      <div></div>
     </div>
   );
 }
