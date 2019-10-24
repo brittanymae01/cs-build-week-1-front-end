3ffc3d6b2d75dca0e18fcaaf441c9d978e0a0467
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-10-24 10:11:21 -0700

    Update DrawRoom.jsx

--- src/components/DrawRoom.jsx
+++ src/components/DrawRoom.jsx
@@ -1,1 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+/* Plan --
+the DrawRoom component would take in the parameters for each individual room,
+draw a box for the center of the room
+draw some little circles on each exit "n","e","s","w"
+*/
+
+function DrawRoom(props) {
+	return <h1> Im an individual room, i have exits to the {props.exit_n} {props.exit_e} {props.exit_s} {props.exit_w}</h1>
+}
+ 
+export default DrawRoom;
