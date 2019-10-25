977e9f35d8a1fa064ccf30ecff43b7c6edc6240a
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-10-25 01:47:17 -0700

    Plan

--- src/components/Core/Game.jsx
+++ src/components/Core/Game.jsx
@@ -13,7 +13,18 @@ export default function Game (props){
 			<div className="logout">
 				<Button onClick={handleLogout}> Log out </Button>
 			</div>
-			
+		
+			<div className="room_data">
+				<h2> We are currently in the _____ Room</h2>
+			</div>
+
+			<div className="room_movement">
+				<p> We can move in these directions </p>
+			</div>
+
+			<div className="room_visualizer">
+				Somehow visualize the rooms
+			</div>
 
 		</div>
 	)
